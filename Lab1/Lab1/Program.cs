3518e5088bcb85224351ae553deb967ad8801b3b
Oliwier Paśko <opasko@edu.cdv.pl>
2025-10-11 16:09:25 +0200

    Lab1 done example

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Example 1
 
-Console.WriteLine("Hello, World!");
+const int requiredAgeFullAccess = 18;
+const int requiredAgeOpenAccess = 14;
+const string accessDenied = "Musisz mieć 18 lat.";
+const string accessAll = "Witamy w naszym sklepie";
+const string accessOpen = "Masz dostęp do skelpu, ale nie mozesz kupić i zarejestrować karty SIM";
+
+int age;
+
+Console.WriteLine("Podaj swój wiek: ");
+
+string input = Console.ReadLine();
+
+bool success = int.TryParse(input, out age);
+
+if (!success)
+{
+    Console.WriteLine("Podaj poprawną wartość!");
+}
+else
+{
+    if (age > requiredAgeFullAccess)
+    {
+        Console.WriteLine(accessAll);
+    } else if (age > requiredAgeOpenAccess)
+    {
+        Console.WriteLine(accessOpen);
+    } else
+    {
+        Console.WriteLine(accessDenied);
+    }
+}
+
+
+// Example 2
+
+// var names = new[] { "Artur", "Alicja", "Michał", "Gosia" };
+//
+// for (int i = 0; i < names.Length; i++)
+// {
+//     Console.WriteLine(names[i]);
+// }
+//
+// foreach (var name in names)
+// {
+//     Console.WriteLine(name);
+// }

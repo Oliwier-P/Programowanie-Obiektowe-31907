08b0a4eb3f8ce586cfc8874b37116f24133cf801
Oliwier Paśko <opasko@edu.cdv.pl>
2025-10-11 16:34:18 +0200

    Lab1 execires done

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,36 +1,36 @@
 ﻿// Example 1
 
-const int requiredAgeFullAccess = 18;
-const int requiredAgeOpenAccess = 14;
-const string accessDenied = "Musisz mieć 18 lat.";
-const string accessAll = "Witamy w naszym sklepie";
-const string accessOpen = "Masz dostęp do skelpu, ale nie mozesz kupić i zarejestrować karty SIM";
-
-int age;
-
-Console.WriteLine("Podaj swój wiek: ");
-
-string input = Console.ReadLine();
-
-bool success = int.TryParse(input, out age);
-
-if (!success)
-{
-    Console.WriteLine("Podaj poprawną wartość!");
-}
-else
-{
-    if (age > requiredAgeFullAccess)
-    {
-        Console.WriteLine(accessAll);
-    } else if (age > requiredAgeOpenAccess)
-    {
-        Console.WriteLine(accessOpen);
-    } else
-    {
-        Console.WriteLine(accessDenied);
-    }
-}
+// const int requiredAgeFullAccess = 18;
+// const int requiredAgeOpenAccess = 14;
+// const string accessDenied = "Musisz mieć 18 lat.";
+// const string accessAll = "Witamy w naszym sklepie";
+// const string accessOpen = "Masz dostęp do skelpu, ale nie mozesz kupić i zarejestrować karty SIM";
+//
+// int age;
+//
+// Console.WriteLine("Podaj swój wiek: ");
+//
+// string input = Console.ReadLine();
+//
+// bool success = int.TryParse(input, out age);
+//
+// if (!success)
+// {
+//     Console.WriteLine("Podaj poprawną wartość!");
+// }
+// else
+// {
+//     if (age > requiredAgeFullAccess)
+//     {
+//         Console.WriteLine(accessAll);
+//     } else if (age > requiredAgeOpenAccess)
+//     {
+//         Console.WriteLine(accessOpen);
+//     } else
+//     {
+//         Console.WriteLine(accessDenied);
+//     }
+// }
 
 
 // Example 2
@@ -45,4 +45,39 @@ else
 // foreach (var name in names)
 // {
 //     Console.WriteLine(name);
+// }
+
+// Zad 1
+// string password;
+//
+// do
+// {
+//     Console.WriteLine("Podaj hasło: ");
+//     password = Console.ReadLine();
+// } while(password != "admin123");
+//
+// Console.WriteLine("Poprawne hasło");
+
+
+// Zad 2
+// int liczba;
+//
+// do
+// {
+//     Console.WriteLine("Podaj liczbe: ");
+//     string input = Console.ReadLine();
+//     bool success = int.TryParse(input, out liczba);
+//     if (!success)
+//     {
+//         Console.WriteLine("Niepraiwdłowa liczba");
+//     }
+// } while(liczba <= 0);
+
+// Zad 3
+
+// var cities = new[] { "London", "Warsaw", "New York", "Kraków", "Poznań" };
+//
+// foreach (var city in cities)
+// {
+//     Console.WriteLine(city);
 // }

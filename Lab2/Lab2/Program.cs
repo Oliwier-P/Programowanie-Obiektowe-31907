c753f19b9f0a8d5671e768b02e5623a21aab4ac7
Oliwier Paśko <opasko@edu.cdv.pl>
2025-10-11 16:57:04 +0200

    Lab2 execires done

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,3 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// // Zad 4
+//
+// Osoba osoba1 = new Osoba("Tom", 34);
+// Osoba osoba2 = new Osoba("Jan", 14);
+// Osoba osoba3 = new Osoba("Kuba", 7);
+// Osoba osoba4 = new Osoba("Julka", 67);
+// Osoba osoba5 = new Osoba("Oli", 18);
+// osoba1.PrzedstawSie();
+// osoba2.PrzedstawSie();
+// osoba3.PrzedstawSie();
+// osoba4.PrzedstawSie();
+// osoba5.PrzedstawSie();
+//
+// class Osoba
+// {
+//     public string Imie;W
+//     public int Wiek;
+//
+//     public Osoba(string imie, int wiek)
+//     {
+//         Imie = imie;
+//         Wiek = wiek;
+//     }
+//     public void PrzedstawSie()
+//     {
+//         Console.WriteLine("Mam na imie "  + Imie + " i mam " + Wiek + " lat");
+//     }
+// }
 
-Console.WriteLine("Hello, World!");
+// Zad 5
+//
+// KontoBankowe konto1 = new KontoBankowe();
+// konto1.Wplata(200);
+// konto1.Wyplata(100);
+// KontoBankowe konto2 = new KontoBankowe();
+// konto2.Wplata(500);
+// konto2.Wyplata(1000);
+//
+// class KontoBankowe
+// {
+//     private double saldo;
+//     
+//     public void Wplata(double kwota) { saldo += kwota; }
+//
+//     public void Wyplata(double kwota)
+//     {
+//         if (saldo >= kwota)
+//         {
+//             saldo -= kwota;
+//             Console.WriteLine("Nowe saldo: " + saldo);
+//         }
+//         else
+//         {
+//             Console.WriteLine("Za mało środków na koncie");
+//         }
+//     }
+// }
+
+// Zad 6
+//
+// Pies pies = new Pies();
+// pies.Jedz();
+// pies.Szczekaj();
+// Kot kot = new Kot();
+// kot.Jedz();
+// kot.Miaucz();
+//
+// class Zwierze
+// {
+//     public void Jedz() => Console.WriteLine("Zwierzę je");
+// }
+// class Pies : Zwierze
+// {
+//     public void Szczekaj() => Console.WriteLine("Hau hau!");
+// }
+// class Kot : Zwierze
+// {
+//     public void Miaucz() => Console.WriteLine("Miauuu!");
+// }
